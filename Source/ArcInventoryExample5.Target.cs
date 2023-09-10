868f51119417f2524ff277e8187eeeaf2eb30944
Garrett Fleenor <kylegar@kylegar.net>
2023-09-10 14:40:15 -0700

    Update to UE5.3

--- Source/ArcInventoryExample5.Target.cs
+++ Source/ArcInventoryExample5.Target.cs
@@ -8,7 +8,7 @@ public class ArcInventoryExample5Target : TargetRules
 	public ArcInventoryExample5Target(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("ArcInventoryExample5");
 	}
--- Source/ArcInventoryExample5Editor.Target.cs
+++ Source/ArcInventoryExample5Editor.Target.cs
@@ -8,7 +8,7 @@ public class ArcInventoryExample5EditorTarget : TargetRules
 	public ArcInventoryExample5EditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("ArcInventoryExample5");
 	}
